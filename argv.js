67c522fd93406aadae26159ee647c9e0009e7e34
Anayan <adarringtonss@gmail.com>
2022-05-02 02:36:35 -0500

    initial spell recording finished

--- argv.js
+++ argv.js
@@ -4,86 +4,95 @@ const enc = require('encoding-japanese') //for testing purposes, remove from fin
 const utf8 = require('utf8');//find fix for broken unicode characters
 const def = require('./definition');
 
-function addToSpell(arr, descr, info) {
-    // console.log(descr)
-    if (descr.includes("-level")) {
-        // console.log("type ", descr.match(/vel (.+)/)[1]);
-        // console.log("level", descr.match(/(.+l?) /)[1])
-        arr.spellType = descr.match(/vel (.+)/)[1];
-        arr.level = descr.match(/(.+l?) /)[1]
-    } /* else if (descr.includes("Higher Levels:")) {
-        // arr[upCast] =*+*
-    } */
+let spellList = {};
+
+function spellBuilder(obj, descr, info = null) {
+    let regType = /-level (\w+) \(|-level (\w+)|(\w+) cantrip/;
+    let spellType = descr.match(regType) ? descr.match(regType) : null;
+    if (descr.includes("-level") && spellType) {
+        obj.level = descr.match(/(.+l?) /)[1];
+        if (spellType[1])
+            obj.ritual = true;
+        obj.spellType = spellType[1] || spellType[2]  
+    } else if (descr.includes("cantrip") && spellType) {
+            obj.level = "cantrip";
+            obj.spellType =  spellType[3];
+    } else if (obj.hasOwnProperty(descr)) 
+        obj[descr] = info;
+     else if (descr.includes("casting time")) 
+        obj.castingTime = info;
+     else if (descr.includes("higher levels")) 
+        obj.upCast = info;
+     else 
+        obj.description = obj.description === "" ? obj.description.concat( '', info.str ) : obj.description.concat( ' ', info.str )
+}
+
+function toSpellList (list = spellList, spell) {
+    console.log(spell)
+    let {spellName, ...spellInfo} = spell;
+    list[spellName] = spellInfo;
 }
 
 let pdfPath = process.argv[2] || "./COFSA.pdf";
-// console.log(pdfPath);
-let pg = [];
 const task = pdfjs.getDocument(pdfPath)
 task.promise
     .then((doc) => {
-        // console.log(enc.detect(doc));
-        doc.getPage(9)
+        doc.getPage(parseInt(process.argv[3]))
             .then((page) => {
                 page.getTextContent()
                     .then((content) => {
-                        // console.log(content.items[Object.keys(content.items)[6]])
                         let list = content.items;
-                        let tmp = new def.Spell;
-                        let tmpArr = [];
+                        let spell = new def.Spell;
                         let debt = 0;
                         const string = list.map( (item, loc) => {
                             let prev = loc - 1;
-                            let ind = tmpArr.length - 1;
+                            let next = loc + 1;
                             if (debt != 0){
+                                console.log("skipped: ", item.str)
                                 debt--;
                                 return;
-                            } else if (loc === 0 ) {
+                            } else if (loc === 0) {
                                 return;
-                            }/* else if(loc >= 50) {
-                                return;
-                            } */ else if (tmpArr.length === 0) {
-                                tmpArr.push(item.str)
-                                tmp.spellName = item.str;
+                            } else if (item.fontName === "g_d0_f1" && item.hasEOL === false) {
+                                console.log(item.str)
+                                spell = new def.Spell;
+                                spell.spellName = item.str;
                             } else if(item.fontName === "g_d0_f1" && list[Object.keys(list)[prev]].fontName != "g_d0_f1") {
-                                // console.log(tmpArr)
-                                console.log(tmp)
-                                tmpArr = []
-                                // obj.spellName = tmpArr[0];
-                                // tmpArr.shift();
-                                // obj.info.push(...tmpArr);
-                                // console.log(obj.info)
-                                // console.log(obj)
-                                // pg.push(obj);
-                                // obj.info = []
-                                // tmpArr = []
+                                toSpellList(spellList, spell);
                             } else {
-                                // console.log(item)
-                                // console.log(item.str)
-                                // console.log((isNaN(list[Object.keys(list)[loc]].str)))
                                 if (list[Object.keys(list)[prev]].hasEOL === false) {
-                                    // console.log('has EOl', item.str)
-                                    tmpArr.push(item.str)  
                                 } else {
-                                    // console.log("Considered for concat", item.str)
-                                    tmpArr[ind] ? tmpArr[ind] = tmpArr[ind].concat( ' ', item.str ) : tmpArr[ind] = item.str
-                                    if ((item.str.includes(":")) || (item.str.includes("-level"))) {
-                                        let info = list[Object.keys(list)[loc + 2]]
-                                        // console.log(info)
-                                        addToSpell(tmp, item.str.toLowerCase(), info.str);
-                                        debt = 2;
-                                        tmpArr[ind] = tmpArr[ind].concat( ' ', list[Object.keys(list)[loc + 2]].str );
+                                    let info;
+                                    if (item.str.includes(":")) {
+                                        let dif = 2;
+                                        if ((list[Object.keys(list)[loc]].str === "Components:") || (list[Object.keys(list)[loc]].str.includes("Higher Levels:"))) {
+                                            info = list[Object.keys(list)[loc + dif]].str;
+                                            while (list[Object.keys(list)[loc + dif]].hasEOL === true) {
+                                                dif++;
+                                                info = info.concat( ' ', list[Object.keys(list)[loc + dif]].str);
+                                            }
+                                        } else {
+                                            info = list[Object.keys(list)[loc + dif]].str
+                                        }
+                                        debt = dif;
+                                        spellBuilder(spell, item.str.toLowerCase().replace(":", ""), info);
+                                        if (!list[Object.keys(list)[next + dif]]) {
+                                            toSpellList(spellList, spell);
+                                        }
+                                    }else {
+                                        spellBuilder(spell, item.str.toLowerCase(), item);
                                     }
                                 }
                             }
-                            if (!list[Object.keys(list)[loc + 1]]) {
-                                // console.log(tmpArr)
+                            if (!list[Object.keys(list)[next]] || !item) {
+                                toSpellList(spellList, spell);
                             }
                         })
-                        pg.forEach(spell => {
-                            console.log(spell.spell)
-                            console.log(spell.info)
-                        })
+                        let spellJson = JSON.stringify(spellList);
+                        const fs = require('fs');
+                        fs.writeFile(process.argv[4], spellJson, function(err, result) {
+                            if(err) console.log('error', err);
+                        });
                     })
             })
     })
--- definition.js
+++ definition.js
@@ -2,13 +2,14 @@ function Spell() {
     this.spellName = '',
     this.level = '',
     this.spellType = '';
-    this.casting = '',
+    this.castingTime = '',
     this.range = '',
     this.components = '',
     this.duration = '',
     this.description = '',
     this.classes = '',
-    this.upCast = ''
+    this.upCast = '',
+    this.isRitual = false;
 }
 
 module.exports = { Spell }
